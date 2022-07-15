6a06d3dda7824c4cd470ccf035aeb18444dcd39a
unknown <tr9836859@gmail.com>
2022-07-15 08:37:24 +0600

    for the review api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
+var jwt = require("jsonwebtoken");
 const cors = require("cors");
 
 // middleware:
@@ -10,6 +10,25 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
+// verify Jwt token:
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized Access" });
+  }
+  // I get token with Bearer from the client side now I remove this Bearer:
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_SECRET_KEY, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+  console.log(token);
+}
 
 // mongoDb:
 
@@ -30,6 +49,13 @@ async function run() {
       .db("mern-manufacturing-bike")
       .collection("purchases");
 
+    const usersCollection = client
+      .db("mern-manufacturing-bike")
+      .collection("users");
+    const reviewsCollection = client
+      .db("mern-manufacturing-bike")
+      .collection("reviews");
+
     app.get("/parts", async (req, res) => {
       const result = await partsCollection.find({}).toArray();
       res.send(result);
@@ -42,7 +68,7 @@ async function run() {
     });
 
     // get individual part:
-    app.get("/part/:id", async (req, res) => {
+    app.get("/part/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
 
       const filter = { _id: ObjectId(id) };
@@ -56,6 +82,52 @@ async function run() {
       const result = await purchasedPartsCollection.insertOne(data);
       res.send(result);
       console.log(data);
+    });
+
+    // post user to the database after login and get a jwt token:
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+
+      // get a jwt token and it's save to the localStorage:
+      const token = jwt.sign({ email: email }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "1h",
+      });
+      res.send({ result, token });
+    });
+
+    // post review by user:
+    app.post("/add-review/:email", verifyJWT, async (req, res) => {
+      // reviewsCollection
+      const email = req.params.email;
+      const decodedEmail = req.decoded.email;
+      //if requested user and decodedEmail both are same:
+
+      if (email === decodedEmail) {
+        const body = req.body;
+        // if user doesn't add any star then not to pass this database:
+        if (body?.ratingNumber > 0) {
+          const result = await reviewsCollection.insertOne(body);
+          return res.send(result);
+        }
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+    });
+
+    app.get("/get-review", verifyJWT, async (req, res) => {
+      const result = await reviewsCollection.find({}).toArray();
+      res.send(result);
     });
   } finally {
   }
