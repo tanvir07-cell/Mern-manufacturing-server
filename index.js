f0e503ea7d0982374890df97738069fe65e84a71
unknown <tr9836859@gmail.com>
2022-07-16 22:30:52 +0600

    for port

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const cors = require("cors");
 
 // app.use(cors({ origin: "*" }));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 // app.use(express.json());
 app.use(bodyParser.json());
 
